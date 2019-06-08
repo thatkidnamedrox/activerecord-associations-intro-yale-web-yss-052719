1a325e0108aacce0fc8d9fcab30d0434cfdc1f58
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-08 20:48:13 +0000

    Automatically backed up by Learn

--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,6 +1,6 @@
 class AddArtistToSongs < ActiveRecord::Migration[4.2]
   def change
     add_column :songs, :artist_id, :integer
-    add_column :songs, :genre_id, :integer
+    # add_column :songs, :genre_id, :integer
   end
 end
